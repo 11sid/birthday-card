81de5313c8bc5a265261f31b6efa8f61952fd624
siddhi suryavanshi <siddhisuryavanshi116@gmail.com>
2024-12-21 14:41:30 +0530

    Added birthday card files

---
+++ script.js
@@ -0,0 +1,14 @@
+const confettiContainer = document.getElementById("confetti-container");
+
+function createConfetti() {
+    const confetti = document.createElement("div");
+    confetti.classList.add("confetti");
+    confetti.style.left = Math.random() * 100 + "vw";
+    confetti.style.animationDuration = Math.random() * 3 + 2 + "s";
+    confettiContainer.appendChild(confetti);
+
+    setTimeout(() => {
+        confetti.remove();
+    }, 5000);
+}
+
